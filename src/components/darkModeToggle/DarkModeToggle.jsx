50db2a57d20a84633d4ea6b945453dd731a0e0d1
abd10225 <abdullahkanzular8@gmail.com>
2025-03-30 14:37:36 +0530

    icon

--- src/components/darkModeToggle/DarkModeToggle.jsx
+++ src/components/darkModeToggle/DarkModeToggle.jsx
@@ -11,6 +11,7 @@ const DarkModeToggle = () => {
         <div className={styles.container} onClick={toggle}>
             <div className={styles.icon}>🌙</div>
             <div className={styles.icon}>🔆</div>
+            <div className={styles.icon}>🔆</div>
             <div
                 className={styles.ball}
                 style={mode === "light" ? { left: "2px" } : { right: "2px" }}
